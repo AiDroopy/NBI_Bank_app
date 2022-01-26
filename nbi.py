7bd7c03895fa95bdde6fbe6fd31ce1cb865b7f63
AiDroopy <bjorn.milebjork@gmail.com>
2022-01-26 16:52:18 +0100

    Another backup

--- nbi.py
+++ nbi.py
@@ -1,9 +1,12 @@
 from dataclasses import dataclass
 from typing import List, Any
 from os import path
-from itertools import count
+import sys
 
 ACCOUNT_PATH = "./accounts/"
+FIRST_ACC_NO = 1001
+DEFAULT_ACC_TYPE = "debit"
+FIRST_CUST_ID = 1111
 
 
 @dataclass()
@@ -15,52 +18,69 @@ class Transaction:
 
 
 class Account:
-    _account_counter = count(1001, 1)
 
-    def __init__(self):
-        self.account_number: int = next(self._account_counter)
-        self.balance: float = 0.0
-        self.account_type: str = "debit"
-        self.transactions: List[Transaction] = None
+    def __init__(self, balance=0.0, acc_no=None, acc_type=DEFAULT_ACC_TYPE):
+        self.balance: float = balance
 
-    def deposit(self, amount, source='ATM'):
+        self.account_number: int = acc_no
+
+        self.account_type: str = acc_type
+        self.transactions: List[Transaction] = []
+
+    def deposit(self, amount: float) -> float:
         self.balance += amount
         return self.balance
 
-    def withdraw(self, amount, destination='ATM'):
-        self.balance -= amount
-        return self.balance
+    def withdraw(self, amount: float) -> float:
+        if self.balance < amount:
+            return 0.0
+        else:
+            self.balance -= amount
+            return self.balance
 
     def show_account(self):
         print(repr(self))
 
     def __repr__(self):
-        return f'Account("{self.account_number}","{self.account_type}",{self.balance}")'
+        return f'Account("{self.account_number}","{self.account_type}","{self.balance}")'
 
 
 class Customer:
-    _customer_counter = count(1111, 1)
 
-    def __init__(self, name, pnr, ids=None):
-        if not ids:
-            self.customer_id: str = str(next(self._customer_counter))
-        else:
-            self.customer_id = ids
-
+    def __init__(self, name: str, pnr: str, ids: int):
+        self.customer_id: int = ids
         self.name: str = name
         self.pnr: str = pnr
         self.accounts: List[Account] = []
+        self.check_accounts()
 
-    def __repr__(self):
-        return f'("{self.customer_id}","{self.name}",{self.pnr}")'
+    def get_accounts(self):
+        with open(ACCOUNT_PATH + str(self.customer_id) + ".txt", 'r') as file:
+            lines = file.readlines()
 
-    def add_account(self, customer):
+            # Create account objects of the data that exists
+        for line in lines:
+
+            acc = line.strip().split(':')
+            # print(acc[0], acc[1], acc[2], "HELLO") # DEBUG
+            account = Account(balance=float(acc[0]), acc_no=int(acc[2]), acc_type=DEFAULT_ACC_TYPE) #FUSK
+            self.accounts.append(account)
+        # print(self.accounts) # DEBUG
+
+    def check_accounts(self):
+        if path.isfile(ACCOUNT_PATH + str(self.customer_id) + ".txt"):
+            self.get_accounts()
+
+    def add_account(self, customer, acc_no: int) -> int:
         """
-
+        Adds an account and
+        :param acc_no: int Account number
         :type customer: Customer
+        :return: int account number
         """
-        new_account = Account()
+        new_account = Account(acc_no=acc_no)
 
+        # Select file mode
         if not path.isfile(ACCOUNT_PATH + str(customer.customer_id) + ".txt"):
             print("There was no file")
             mode = 'wt'
@@ -68,60 +88,50 @@ class Customer:
             print("File already exists")
             mode = 'at'
 
-        with open(ACCOUNT_PATH + str(new_account.account_number), mode) as file:
-            file.writelines(str(new_account.account_number) + ":" + str(new_account.account_type) + ":" +
-                            str(new_account.balance) + "\n")
+        with open(ACCOUNT_PATH + str(customer.customer_id) + ".txt", mode) as file:
+            file.writelines(str(new_account.balance) + ":" + str(new_account.account_type) + ":" +
+                            str(new_account.account_number) + "\n")
 
         customer.accounts.append(new_account)
         print(customer.accounts)
 
         return new_account.account_number
 
+    def end_account(self, acc_no: int) -> float:
+        # print("Customer.end_account", acc_no)  # DEBUG
+        balance = 0.0
+        for obj in self.accounts:
+            # print(obj.account_number, "NEW")  # DEBUG
+            if obj.account_number == acc_no:
+                balance = obj.balance
+
+        # END THE ACCOUNT HERE!!!
+        return balance
+
+
+
+    def __repr__(self):
+        return f'("{self.customer_id}","{self.name}",{self.pnr}")'
+
 
 class Bank:
     def __init__(self):
         self.customers = []
+        self.last_account: int = FIRST_ACC_NO
+        self.last_customer: int = FIRST_CUST_ID
+        self._init()
         self._load()
 
-    def show_main_menu(self):
-        print("Welcome to your simple and fast bank system from NBI")
-        print("Select one of the following options to enter our service:")
-        print()
-        print("1. Add a new customer\t\t 2. View all customers")
-        print("3. View a customer (pnr)  9. Quit NBI Bank app!")
-
-        menu_val = int(input("Enter (1-4): "))
-
-        if menu_val == 1:
-            self.new_customer_menu()
-
-        elif menu_val == 2:
-            self.get_customers()
-
-        elif menu_val == 3:
-            print(menu_val)
-        elif menu_val == 4:
-            print(menu_val)
+    def _init(self):
+        # Create or read a state file for saving/loading the state of the bank
+        if not path.isfile("./state.txt"):
+            with open('state.txt', 'w') as state_file:
+                state_file.writelines(str(FIRST_ACC_NO) + "," + str(FIRST_CUST_ID))
         else:
-            print("Wrong input")
-            self.show_main_menu()
-
-    def customers_menu(self):
-        print()
-        print("1. Manage a customer\t\t 2. Remove a customer")
-        print("3. Back to main menu")
-        print()
-        menu_val = int(input("Enter (1-3): "))
-
-        if menu_val == 1:
-            self.manage_customer(int(input("Enter row number: ")) - 1)
-        elif menu_val == 2:
-            self.remove_customer(row=int(input("Enter row number: ")) - 1)
-        elif menu_val == 3:
-            pass
-        else:
-            print("Wrong input")
-            self.customers_menu()
+            with open('state.txt', 'r') as state_file:
+                state = state_file.readline().split(',')
+            self.last_account = int(state[0])
+            self.last_customer = int(state[1])
 
     def _load(self):
         """
@@ -136,17 +146,18 @@ class Bank:
         with open('bank.txt', 'r') as file:
             lines = file.readlines()
 
-        # Create customer object of the data that exists
+        # Create customer object of the data that/if exists
         for line in lines:
             line_count += 1
             cust = line.strip().split(':')
-            # print(cust)
+
             customer = Customer(name=cust[0], pnr=cust[1], ids=cust[2])
             self.customers.append(customer)
 
-        # If the file is empty, create at least one customer
+        # If the file is empty, create at least one customer and create a state file
         if line_count < 1:
             print("This bank has no customers, please add one or more to use the bank.")
+
             self.new_customer_menu()
             self.show_main_menu()
         else:
@@ -167,36 +178,48 @@ class Bank:
 
         return self.customers
 
-    def new_customer_menu(self):
-        name = input("Enter the customers name: ")
-        pnr = input("Enter customers security no: ")
-        ret = self.add_customer(name, pnr)
-
-        if ret:
-            print("Customer created")
-            self.show_main_menu()
-        else:
-            print("Customer already a client of the bank")
-            self.show_main_menu()
-
-    def add_customer(self, name, pnr):
+    @staticmethod
+    def customer_exists(pnr: str) -> bool:
         """
-        Adds a customer to the bank.txt file. Returns True on success False if not.
-        :param name: <string>
-        :param pnr: <string>
+        Checks if the customer is already in system
+        :param pnr: str security number
         :return: bool
         """
+        for line in open('bank.txt').readlines():
+            cust = line.strip().split(':')
+            if str(cust[1]) == pnr:
+                return True
 
-        # Create a new customer:
-        new_customer = Customer(name, pnr)
+        return False
 
-        # Serialize and save the new customer
-        with open('bank.txt', 'at') as file:
-            file.writelines(str(new_customer.name) + ":" + new_customer.pnr + ":" + new_customer.customer_id + "\n")
+    def add_customer(self, name: str, pnr: str) -> bool:
+        """
+        Adds a customer to the bank.txt file. Returns True on success False if not.
+        :param name: str
+        :param pnr: str
+        :return: bool
+        """
+        # Check if security no is already registered in bank
+        if self.customer_exists(pnr):
+            print("The customer is already a client")
+            return False
 
-        # Update the customers
-        self.customers.append(new_customer)
-        return True
+        else:
+            # Create a new customer:
+            new_customer = Customer(name, pnr, self.last_customer)
+            self.last_customer = self.last_customer + 1
+
+            # Serialize and save the new customer
+            with open('bank.txt', 'at') as file:
+                file.writelines(str(new_customer.name) + ":" + new_customer.pnr + ":" + str(new_customer.customer_id) +
+                                "\n")
+
+            # Update the customers
+            self.customers.append(new_customer)
+
+            # Update state file
+            self.update_data_source()
+            return True
 
     def get_customer(self, pnr):
         """
@@ -225,34 +248,14 @@ class Bank:
         :return: list<string> all accounts removed and total funds that will be paid out.
         """
         if pnr:
-            print("Delete by pnr")
+            print("Delete by pnr")  # DEBUG
 
         elif row:
-            print("Delete by row")
+            print("Delete by row", row)  # DEBUG
+
 
         else:
-            print("Nothing to delete")
-
-    def add_account(self, pnr):
-        """
-        Adds a account to existing customer
-        :param pnr: <String>
-        :return: <int> account number if success, -1 if not.
-        """
-
-        # Find the customer object that needs to bee changed
-
-        for cust in self.customers:
-            if cust.pnr == str(pnr):
-                acc_no = cust.add_account(cust)
-                print(acc_no)
-
-        return  # new_account.account_number
-
-    def update_data_source(self):
-        with open('bank2.txt', 'w', encoding='utf-8') as file:
-            for elem in self.customers:
-                file.write('{} {}\n'.format(elem[0], ' '.join(str(i) for i in elem[1])))
+            print("Nothing to delete")  # DEBUG
 
     def manage_customer(self, pos):
         """
@@ -267,13 +270,113 @@ class Bank:
         print("Customer name: {} \t\t\t Customer id: {}".format(customer.name, customer.customer_id))
         print("Security number: {}".format(customer.pnr))
         print()
+        print("Accounts:")
+
+        # Load accounts
+        account_file = ACCOUNT_PATH + str(customer.customer_id) + ".txt"
+        if path.isfile(account_file):
+            # print("THERE WAS AN ACCOUNT FILE", account_file)
+            for i, line in enumerate(open(account_file).readlines()):
+                acc = line.strip().split(':')
+                print(str(i+1) + ". Account number:", str(acc[2]) + "\t\t" + "Balance:", str(acc[0]) + "\t" + "Type:" +
+                      str(acc[1]))
+        else:
+            print("No accounts registered!")
+
         self.customer_menu(customer)
 
-    def customer_menu(self, customer):
-        print(customer.pnr)
+    def add_account(self, pnr: str) -> int:
+        """
+        Adds a account to existing customer
+        :param pnr: str
+        :return: int account number if success, -1 if not.
+        """
+
+        # Find the customer object that needs to bee changed
+        try:
+            ret = 0
+            for cust in self.customers:
+                if cust.pnr == str(pnr):
+                    acc_no = cust.add_account(cust, self.last_account)
+                    ret = self.last_account
+                    self.last_account += 1
+                    print(acc_no)
+
+            self.update_data_source()
+            return ret
+        except:
+            print("Something went wrong!")
+            return -1
+
+    def update_data_source(self):
+        with open('state.txt', 'w') as state_file:
+            state_file.writelines(str(self.last_account) + "," + str(self.last_customer))
+
+    def show_main_menu(self):
+        print("Welcome to your simple and fast bank system from NBI")
+        print("Select one of the following options to enter our service:")
+        print()
+        print("Main menu:")
+        print("1. Add a new customer\t\t 2. View all customers")
+        print("3. View a customer(pnr)\t\t 9. Quit NBI Bank app!")
+
+        menu_val = int(input("Enter (1-4): "))
+
+        if menu_val == 1:
+            self.new_customer_menu()
+        elif menu_val == 2:
+            self.get_customers()
+        elif menu_val == 3:
+            print(menu_val)
+        elif menu_val == 4:
+            print(menu_val)
+        elif menu_val == 9:
+            print("God bye :)")
+            sys.exit(0)
+        else:
+            print("Wrong input")
+            self.show_main_menu()
+
+    def new_customer_menu(self):
+        print()
+        print("New customer menu:")
+        print()
+
+        name = input("Enter the customers name: ")
+        pnr = input("Enter customers security no: ")
+        ret = self.add_customer(name, pnr)
+
+        if ret:
+            print("Customer created")
+            self.show_main_menu()
+        else:
+            print("Customer already a client of the bank")
+            self.show_main_menu()
+
+    def customers_menu(self):
+        print()
+        print("Customers menu:")
+        print("1. Manage a customer\t\t 2. Remove a customer")
+        print("3. Back to main menu")
+        print()
+        menu_val = int(input("Enter (1-3): "))
+
+        if menu_val == 1:
+            self.manage_customer(int(input("Enter row number: ")) - 1)
+        elif menu_val == 2:
+            self.remove_customer(row=int(input("Enter row number: ")) - 1)
+        elif menu_val == 3:
+            pass
+        else:
+            print("Wrong input")
+            self.customers_menu()
+
+    def customer_menu(self, customer: Customer):
+        # print(customer.pnr)  # DEBUG
+        print()
         print("1. Change customer name \t\t\t 2. Create an account")
-        print("2. Delete an account \t\t\t\t 4. View transactions")
-        print("9. Back to main menu")
+        print("3. Delete an account \t\t\t\t 4. Make a withdraw")
+        print("5. Make a deposit \t\t\t\t\t9. Back to main menu")
         menu_val = int(input("Enter (1-9): "))
 
         if menu_val == 1:
@@ -285,13 +388,20 @@ class Bank:
             else:
                 print("Something went wrong!", customer.pnr, new_name)
 
-        elif menu_val == 2:
+        elif menu_val == 2:  # Create account
             self.add_account(customer.pnr)
 
-        elif menu_val == 3:
-            pass
+        elif menu_val == 3:  # Delete account
+            print(customer.accounts)
+            acc_no = int(input("Enter account number: "))
+            balance = customer.end_account(acc_no)
+            print("Total out: " + str(balance))
 
-        elif menu_val == 4:
+        elif menu_val == 4:  # Withdraw
+            acc_no = int(input("Enter account number: "))
+            amount = int(input("Enter amount: "))
+
+        elif menu_val == 5:  # Deposit
             pass
 
         elif menu_val == 9:
@@ -300,3 +410,4 @@ class Bank:
         else:
             print("Wrong input")
             self.customer_menu(customer)
+
